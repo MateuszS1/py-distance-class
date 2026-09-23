fe16444fa5acd17091eeb78de061c31f02aba581
Mateusz Sobolak <mateusz135755@wp.com>
2026-09-23 16:49:23 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,92 @@
+from __future__ import annotations
+from typing import Any
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int | float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Any) -> Any:
+        other_km = Distance.return_km_value(other)
+
+        if other_km is not None:
+            return Distance(self.km + other_km)
+
+        return other_km
+
+    def __iadd__(self, other: Any) -> Any:
+        other_km = Distance.return_km_value(other)
+
+        if other_km is not None:
+            self.km += other_km
+            return self
+
+        return other_km
+
+    def __mul__(self, other: Any) -> Any:
+        if isinstance(other, int | float):
+            return Distance(self.km * other)
+
+        return None
+
+    def __truediv__(self, other: Any) -> Any:
+        if isinstance(other, int | float):
+            return Distance(round(self.km / other, 2))
+
+        return None
+
+    def __lt__(self, other: Any) -> Any:
+        other_km = Distance.return_km_value(other)
+
+        if other_km is not None:
+            return True if self.km < other_km else False
+
+        return other_km
+
+    def __gt__(self, other: Any) -> Any:
+        other_km = Distance.return_km_value(other)
+
+        if other_km is not None:
+            return True if self.km > other_km else False
+
+        return other_km
+
+    def __eq__(self, other: Any) -> Any:
+        other_km = Distance.return_km_value(other)
+
+        if other_km is not None:
+            return True if self.km == other_km else False
+
+        return other_km
+
+    def __le__(self, other: Any) -> Any:
+        other_km = Distance.return_km_value(other)
+
+        if other_km is not None:
+            return True if self.km <= other_km else False
+
+        return other_km
+
+    def __ge__(self, other: Any) -> Any:
+        other_km = Distance.return_km_value(other)
+
+        if other_km is not None:
+            return True if self.km >= other_km else False
+
+        return other_km
+
+    @staticmethod
+    def return_km_value(other: Any) -> Any:
+        if isinstance(other, Distance):
+            return other.km
+
+        elif isinstance(other, int) or isinstance(other, float):
+            return other
+
+        return None
